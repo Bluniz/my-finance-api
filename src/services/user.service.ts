0afe0ecc6019a8840a59bb2ba3329ff2f31509fc
Lucas Rosa <lucassantosrosa51@gmail.com>
2022-06-10 15:18:21 -0300

    chore: Validation of create user with same email

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,6 +12,16 @@ export class UserService {
   constructor(@Inject("PRISMA_SERVICE") private prisma: PrismaService) {}
 
   async createUser({ name, email }: CreateUserParams) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new Error("Already exists a user with the same email");
+    }
+
     return this.prisma.user.create({
       data: {
         id: new ObjectID().toString(),
