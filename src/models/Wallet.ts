8b9bd1f416e2d6931fd270cb8cc60927d6d528b3
Lucas Rosa <lucassantosrosa51@gmail.com>
2022-06-15 10:58:13 -0300

    chore: Add Wallet in Schema

---
+++ src/models/Wallet.ts
@@ -0,0 +1,17 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import { Purchase } from "./Purchase";
+
+@ObjectType("Wallet")
+export class Wallet {
+  @Field((_type) => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  dueDate: number;
+
+  @Field(() => [Purchase])
+  purchases: Purchase[];
+}
